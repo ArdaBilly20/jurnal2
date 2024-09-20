4dccf35fe1be9099b2fa532679b802ef21d4f746
ArdaBilly20 <muh.ardabillyanugrah@gmail.com>
2024-09-20 15:44:47 +0800

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public class helloworld {
+
+	public static vold main(string[] args) {
+		system.out.println("Hello Muhammad ArdaBilly Anugrah");
+		system.out.println("Nim:607062400104");
+		system.out.println("D3IF4801");
+	}
+}
